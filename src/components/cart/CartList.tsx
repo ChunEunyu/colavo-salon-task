dacb2fe4e03e89bf67e1a78b80d2df0c165f8b60
ChunEunyu <xhoneyx01@naver.com>
2024-10-02 01:15:49 +0900

    feat: 장바구니에 담은 항목들을 보여주는 컴포넌트

---
+++ src/components/cart/CartList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import CartItemCard from './CartItemCard';
+import { CartItemsType, CartDiscountsType } from '../../types';
+import CartDiscountCard from './CartDiscountCard';
+
+interface IProps {
+  cartItems: CartItemsType;
+  cartDiscounts: CartDiscountsType;
+}
+
+function CartList({ cartItems, cartDiscounts } : IProps ) {
+  return (
+    <div>
+      {cartItems.map((item, index) => (
+        <CartItemCard 
+          key={index}
+          item={item}
+        />
+      ))}
+      {cartDiscounts.map((discount, index) => (
+        <CartDiscountCard 
+          key={index}
+          discount={discount}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CartList;
