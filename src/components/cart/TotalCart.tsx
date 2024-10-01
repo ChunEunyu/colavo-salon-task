28d9079c0ef6b995be26f9211adab37539a44fa0
ChunEunyu <xhoneyx01@naver.com>
2024-10-02 01:07:10 +0900

    feat: 장바구니 총 가격을 보여주는 컴포넌트

---
+++ src/components/cart/TotalCart.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useCartStore } from '../../stores/cartStore';
+import { useCurrencyStore } from '../../stores/currencyCodeStroe';
+import { getFormatPrice } from '../../utils/getFormatPrice';
+import { getFinalPrice } from '../../utils/getPrice';
+
+function TotalCart() {
+  const { currencyCode } = useCurrencyStore();
+  const { cartItems, cartDiscounts } = useCartStore();
+  const totalPrice = getFinalPrice(cartItems, cartDiscounts);
+
+  const handleClick = () => {
+    alert('완료!')
+  }
+
+  return (
+    <div className='absolute left-0 right-0 bottom-3 w-full'>
+      <div className='flex justify-between items-center my-4 mx-5'>
+        <p className='text-deep-gray'>
+          합계
+        </p>
+        <p className='text-3xl'>
+          {getFormatPrice(totalPrice, currencyCode)}
+        </p>
+      </div>
+      <div className='flex justify-center'>
+        <button 
+          onClick={handleClick}
+          className='w-11/12 bg-purple text-white py-3 rounded-md'
+        >
+          완료
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TotalCart;
