8d6e21363f8a99c339e4bcc887e077fa139af063
ChunEunyu <xhoneyx01@naver.com>
2024-10-02 01:19:12 +0900

    style: 커스텀 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'purple': '#9885f0',
+      'baby-pink':'#fdf1f5',
+      'deep-pink': '#ff94c5',
+      'light-gray':'#ededed',
+      'gray': '#d2d7db',
+      'deep-gray': '#9ca0a6',
+      'purple-gray': '#69657c',
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
