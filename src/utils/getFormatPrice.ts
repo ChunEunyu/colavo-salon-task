e5c41ddffcd00282fc72dfb3571906d5c79c0230
ChunEunyu <xhoneyx01@naver.com>
2024-10-02 01:04:29 +0900

    feat: currency_code에 따른 가격 포맷 함수

---
+++ src/utils/getFormatPrice.ts
@@ -0,0 +1,28 @@
+export function getFormatPrice(price: number, currencyCode: string): string {
+    let formattedPrice = '';
+  
+    if (currencyCode === 'KRW') {
+      const formatter = new Intl.NumberFormat('ko-KR', {
+        style: 'decimal'
+      });
+
+      formattedPrice = `${formatter.format(price)}원`; 
+
+    } else if (currencyCode === 'USD') {
+      const formatter = new Intl.NumberFormat('en-US', {
+        style: 'decimal'
+      });
+      
+      formattedPrice = `${formatter.format(price)}`; 
+
+    } else {
+      const formatter = new Intl.NumberFormat(undefined, {
+        style: 'decimal'
+      });
+
+      formattedPrice = formatter.format(price); 
+    }
+  
+    return formattedPrice;
+}
+  
