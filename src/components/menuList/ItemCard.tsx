5b1496d18e7a166759e8b8910825c087d95c4b85
ChunEunyu <xhoneyx01@naver.com>
2024-10-02 01:08:29 +0900

    feat: 시술 메뉴 리스트에 사용될 카드 컴포넌트

---
+++ src/components/menuList/ItemCard.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { getFormatPrice } from '../../utils/getFormatPrice'
+import { Item, Items } from '../../types';
+import { useCurrencyStore } from '../../stores/currencyCodeStroe';
+import { useCartStore } from '../../stores/cartStore';
+import CheckIcon from '../../common/CheckIcon';
+
+interface IProps {
+  item: Item;
+  itemKey: string;
+}
+
+function ItemCard({ item, itemKey }: IProps) {
+  const { cartItems, setCartItems } = useCartStore();
+  const { currencyCode } = useCurrencyStore();
+
+  const exist = cartItems.some(item => item[itemKey] !== undefined);
+  const [checked, setChecked] = useState<boolean>(exist);
+ 
+  const handleClick = () => {
+    const itemObject: Items = { [itemKey] : item };
+    
+    if (checked) {
+      const updated = cartItems.filter(item => !item[itemKey])
+      setCartItems(updated);
+      setChecked(false);
+    } else {
+      setCartItems([ ...cartItems, itemObject])
+      setChecked(true);
+    }
+  }
+  
+  return (
+    <div onClick={handleClick}>
+      <div className='flex justify-between items-center my-4 mx-1'>
+        <div className='w-5/6'>
+          <p className='font-semibold'>
+            {item.name}
+          </p>
+          <p className='text-sm text-deep-gray'>
+            {getFormatPrice(item.price, currencyCode)}
+          </p>
+        </div>
+        { checked && <CheckIcon /> }
+      </div>
+      <div className='bg-light-gray w-full h-[0.03rem]' />
+    </div>
+  );
+}
+
+export default ItemCard;
